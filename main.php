d2afa162d342237a02849bc26eb5a2ecce396000
Moaane <gpro0150@gmail.com>
2023-02-22 15:25:52 +0700

    update

--- main.php
+++ main.php
@@ -1,26 +1,34 @@
-<?php 
-    require_once 'header.php';
+<?php
+require_once 'header.php';
 ?>
 
-    
-    <?php 
-        $page = $_GET['page'];
 
-            switch ($page) {
-                case 'home':
-                    require_once 'home.php';
-                    break;
-                
-                case 'about':
-                    require_once 'about.php';
-                    break;
-                    
-                default:
-                    require_once 'error.php';
-                    break;
-            }
-    ?>
+<?php
+$page = $_GET['page'];
+require_once 'functions.php';
+switch ($page) {
+    case 'home':
+        require_once 'home.php';
+        break;
 
-    <<?php 
-        require_once 'footer.php';
+    case 'about':
+        require_once 'about.php';
+        break;
+
+    case 'detail':
+        require_once 'detail.php';
+        break;
+
+    case 'ulasan':
+        require_once 'ulasan.php';
+        break;
+
+    default:
+        require_once 'error.php';
+        break;
+}
+?>
+
+<<?php
+    require_once 'footer.php';
     ?>
